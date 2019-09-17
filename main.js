5002fa88b2f08d3a5bc995cb5a8d4c11bc9ef0ba
Cezary Wakuliński <cezary.wakulinski@conversion.pl>
2019-09-17 15:07:58 +0200

    Add getAvailableMoves method for pawn, king, rook

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
 let app = void 0;
 let chessPieces = { 
     king : {
-        asset: 'Assets/chess-king.svg'
+        asset: 'Assets/chess-king.svg',
+        getAvailableMoves: (cordsConfig) => {
+            let moveCors = []
+            const targetFigure = app.getFigureByCords(cordsConfig)
+
+            for(let Xindex = -1; Xindex < 2; Xindex++) {
+                for(let Yindex = -1; Yindex < 2; Yindex++) {
+                    if(Xindex || Yindex){
+                        let moveCords = {}
+
+                        
+                        moveCords.x = cordsConfig.x + Xindex
+                        moveCords.y = cordsConfig.y + Yindex
+    
+                        if(moveCords.x >= 0 && moveCords.x < 8 && moveCords.y >= 0 && moveCords.y < 8){
+                            const cordsFigure = app.getFigureByCords(moveCords)
+
+                                if(cordsFigure && cordsFigure.color !== targetFigure.color) {
+                                    moveCords.isEnemy = true
+                                }
+
+                                if(cordsFigure.color !== targetFigure.color || !cordsFigure) {
+                                    moveCors.push(moveCords)
+                                }       
+                        }
+                    }
+                }
+            }
+            
+            if(moveCors.length){
+                return moveCors
+            } else {
+                return false
+            }
+        }
     },
 
     queen: {
@@ -9,7 +43,58 @@ let chessPieces = {
     },
 
     rook: {
-        asset: 'Assets/chess-rook.svg'
+        asset: 'Assets/chess-rook.svg',
+        getAvailableMoves: (cordsConfig) => {
+            const targetFigure = app.getFigureByCords(cordsConfig)
+            
+            const getStragightFields = (direction, axis) => {
+                let moveCors = []
+                if(!((direction === 'up' && cordsConfig[axis] === 7) || (direction === 'down' && cordsConfig[axis] === 0))) {
+                    let conditionNumber = 8
+                    let operation = 1
+
+                    if(direction === 'down') {
+                        conditionNumber = -1
+                        operation = -1
+                    }
+
+                   
+
+                    for(iteration = cordsConfig[axis]+operation; iteration !== conditionNumber;iteration = iteration + operation) {
+                        let possibleCords = {...cordsConfig}
+                        possibleCords[axis] = iteration
+
+                        let possiebleFigure = app.getFigureByCords(possibleCords)
+
+                        if(possiebleFigure) {
+                            if(possiebleFigure.color !== targetFigure.color) {
+                                possibleCords.isEnemy = true
+                                moveCors.push(possibleCords)
+                            }
+
+                            break
+                        } else {
+                            moveCors.push(possibleCords)
+                        }
+
+                        
+                    }
+                }
+
+                return moveCors
+            }
+
+            const getFields = (checkingAxis) => {
+                moveCors = [ ...getStragightFields('up', checkingAxis), ...getStragightFields('down', checkingAxis)]
+                return moveCors
+            }
+
+            return [
+                ...getFields('x'),
+                ...getFields('y')
+            ]
+            
+        }
 
     },
 
@@ -22,7 +107,27 @@ let chessPieces = {
     },
 
     pawn: {
-        asset: 'Assets/chess-pawn.svg'
+        asset: 'Assets/chess-pawn.svg',
+        getAvailableMoves: (cordsConfig) => {
+            let moveCors = []
+            const targetFigure = app.getFigureByCords(cordsConfig)
+            const addValue = targetFigure.color === 'white' ? 1 : -1
+
+            moveCors.push({
+                ...cordsConfig,
+                y : cordsConfig.y + addValue
+            })
+
+
+            if((targetFigure.color === 'black' && cordsConfig.y === 6) || (targetFigure.color === 'white' && cordsConfig.y === 1)) {
+                moveCors.push({
+                    ...cordsConfig,
+                    y : cordsConfig.y + 2 * addValue
+                })
+            }
+
+            return moveCors
+        }
     },
 
     figures: {
