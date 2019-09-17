ab2355bef92458535b8abcfd66bf8165b7071c86
Cezary Wakuliński <cezarywakulinskiv@gmail.com>
2019-09-17 20:30:22 +0200

    Webpack now can handle typescript + babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     mode: "development",
-    entry: "./main.js",
+    entry: "./main.ts",
     watch: true,
     output: {
         path: path.resolve(__dirname, "webpack-output"),
@@ -19,6 +19,11 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.ts$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [ 'babel-loader', 'ts-loader']
             }
         ]
     }
