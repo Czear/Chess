423c5bfd729270c57e4b81e46909f10eaf954c2c
Cezary Wakuliński <cezary.wakulinski@conversion.pl>
2019-09-17 15:02:29 +0200

    Add base logic setup

---
+++ main.js
@@ -0,0 +1,37 @@
+let app = void 0;
+let chessPieces = { 
+    king : {
+        asset: 'Assets/chess-king.svg'
+    },
+
+    queen: {
+        asset: 'Assets/chess-queen.svg'
+    },
+
+    rook: {
+        asset: 'Assets/chess-rook.svg'
+
+    },
+
+    bishop: {
+        asset: 'Assets/chess-bishop.svg',
+    },
+
+    knight: {
+        asset: 'Assets/chess-knight.svg'
+    },
+
+    pawn: {
+        asset: 'Assets/chess-pawn.svg'
+    },
+
+    figures: {
+
+    }
+}
+
+class ChessGame {
+}
+
+class Piece extends ChessGame {
+}
