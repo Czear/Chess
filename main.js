191305632e8ad4d1496d0aa4b57b5ee957bae1af
Cezary Wakuliński <cezary.wakulinski@conversion.pl>
2019-09-17 15:06:24 +0200

    Add figure utilities

--- main.js
+++ main.js
@@ -131,6 +131,61 @@ class ChessGame {
 
         return figure
     }
-}
+}
+
 class Piece extends ChessGame {
-}
+    constructor(pieceType, color) {
+        super();
+        this.pieceType = pieceType
+        this.color = color
+        this.isActive = true
+        this.getAvailableMoves = chessPieces[pieceType.replace(/-\d/, '')].getAvailableMoves
+    }
+
+    logFigure() {
+        console.log(this)
+    }
+
+    move(cordsConfig) {
+        const figureElement = this.getFigureDOMElement
+        const figureCords = this.getFigureCords
+        const finishMoveSequence = () => {
+            this.getFigureDOMElement.style.transform = ''
+            this.getFigureDOMElement.classList.remove('transforming')
+            app.getElemenyByCords(cordsConfig).appendChild(this.getFigureDOMElement)
+            figureElement.removeEventListener('transitionend', finishMoveSequence)
+        }
+
+        figureElement.addEventListener('transitionend', finishMoveSequence)
+
+        this.getFigureDOMElement.classList.add('transforming')
+        this.getFigureDOMElement.style.transform = `translateY(${100 * (figureCords.y - cordsConfig.y)}%) translateX(${100 * (cordsConfig.x - figureCords.x)}%)`
+    }
+
+    get getPieceElement() {
+        let pieceElement = document.createElement('div')
+        let pieceImg = document.createElement('img')
+
+        pieceElement.className = 'chess-figure '
+        pieceElement.dataset.color = this.color
+        pieceElement.dataset.type = this.pieceType
+
+        pieceImg.src = chessPieces[this.pieceType] ? chessPieces[this.pieceType].asset : chessPieces[this.pieceType.replace(/-[0-9]*/, '')].asset
+        pieceElement.appendChild(pieceImg)
+
+        return pieceElement
+    }
+
+    get getFigureDOMElement () {
+        return  document.querySelector(`[data-color="${this.color}"][data-type="${this.pieceType}"]`);
+    }
+
+    get getFigureCords() {
+        const rowElement = this.getFigureDOMElement.parentElement.parentElement
+
+        return {
+            x: nodeListToArray(rowElement.querySelectorAll('.figure-field')).indexOf( this.getFigureDOMElement.parentElement ),
+            y: nodeListToArray(rowElement.parentElement.querySelectorAll('.chess-row')).reverse().indexOf( rowElement )
+        }
+    }
+}
