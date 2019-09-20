e50ae0d35e9f6a4f1934f970a6c90bb5851d9163
Cezary Wakuliński <cezary.wakulinski@conversion.pl>
2019-09-21 00:00:05 +0200

    Refactor main file into typescript

--- main.ts
+++ main.ts
@@ -1,55 +1,56 @@
+import { Utility } from '@Scripts/utilities'
+import { Board } from '@Scripts/board'
+
 require('./main.scss')
 
-let app = void 0;
-let chessPieces = { 
+export let chessPieces: chessPieces = { 
     king : {
         asset: 'Assets/chess-king.svg',
-        getAvailableMoves: (cordsConfig) => {
+        getAvailableMoves: cordsConfig => {
             let moveCors = []
-            const targetFigure = app.getFigureByCords(cordsConfig)
+            const targetFigure = Utility.getFigureByCords(cordsConfig)
 
             for(let Xindex = -1; Xindex < 2; Xindex++) {
                 for(let Yindex = -1; Yindex < 2; Yindex++) {
                     if(Xindex || Yindex){
-                        let moveCords = {}
+                        let moveCords: Cords = {x: 0, y: 0};
 
                         
                         moveCords.x = cordsConfig.x + Xindex
                         moveCords.y = cordsConfig.y + Yindex
     
                         if(moveCords.x >= 0 && moveCords.x < 8 && moveCords.y >= 0 && moveCords.y < 8){
-                            const cordsFigure = app.getFigureByCords(moveCords)
+                            const cordsFigure = Utility.getFigureByCords(moveCords)
 
-                                if(cordsFigure && cordsFigure.color !== targetFigure.color) {
+                                if(targetFigure && cordsFigure && cordsFigure.color !== targetFigure.color) {
                                     moveCords.isEnemy = true
                                 }
 
-                                if(cordsFigure.color !== targetFigure.color || !cordsFigure) {
+                                if(!cordsFigure || (targetFigure && cordsFigure && cordsFigure.color !== targetFigure.color)) {
                                     moveCors.push(moveCords)
-                                }       
+                                }     
                         }
                     }
                 }
             }
             
-            if(moveCors.length){
-                return moveCors
-            } else {
-                return false
-            }
+            return moveCors
         }
     },
 
     queen: {
-        asset: 'Assets/chess-queen.svg'
+        asset: 'Assets/chess-queen.svg',
+        getAvailableMoves: cordsConfig => {
+            return [cordsConfig]
+        }
     },
 
     rook: {
         asset: 'Assets/chess-rook.svg',
-        getAvailableMoves: (cordsConfig) => {
-            const targetFigure = app.getFigureByCords(cordsConfig)
+        getAvailableMoves: cordsConfig => {
+            const targetFigure = Utility.getFigureByCords(cordsConfig)
             
-            const getStragightFields = (direction, axis) => {
+            const getStragightFields = (direction: Direction, axis: Axis) => {
                 let moveCors = []
                 if(!((direction === 'up' && cordsConfig[axis] === 7) || (direction === 'down' && cordsConfig[axis] === 0))) {
                     let conditionNumber = 8
@@ -60,16 +61,14 @@ let chessPieces = {
                         operation = -1
                     }
 
-                   
-
-                    for(iteration = cordsConfig[axis]+operation; iteration !== conditionNumber;iteration = iteration + operation) {
+                    for(let iteration = cordsConfig[axis]+operation; iteration !== conditionNumber;iteration = iteration + operation) {
                         let possibleCords = {...cordsConfig}
                         possibleCords[axis] = iteration
 
-                        let possiebleFigure = app.getFigureByCords(possibleCords)
+                        let possibleFigure = Utility.getFigureByCords(possibleCords)
 
-                        if(possiebleFigure) {
-                            if(possiebleFigure.color !== targetFigure.color) {
+                        if(possibleFigure && targetFigure) {
+                            if(possibleFigure.color !== targetFigure.color) {
                                 possibleCords.isEnemy = true
                                 moveCors.push(possibleCords)
                             }
@@ -86,8 +85,8 @@ let chessPieces = {
                 return moveCors
             }
 
-            const getFields = (checkingAxis) => {
-                moveCors = [ ...getStragightFields('up', checkingAxis), ...getStragightFields('down', checkingAxis)]
+            const getFields = (checkingAxis: 'x' | 'y') => {
+                const moveCors = [ ...getStragightFields('up', checkingAxis), ...getStragightFields('down', checkingAxis)]
                 return moveCors
             }
 
@@ -102,30 +101,39 @@ let chessPieces = {
 
     bishop: {
         asset: 'Assets/chess-bishop.svg',
+        getAvailableMoves: cordsConfig => {
+            return [cordsConfig]
+        }
     },
 
     knight: {
-        asset: 'Assets/chess-knight.svg'
+        asset: 'Assets/chess-knight.svg',
+        getAvailableMoves: cordsConfig => {
+            return [cordsConfig]
+        }
     },
 
     pawn: {
         asset: 'Assets/chess-pawn.svg',
-        getAvailableMoves: (cordsConfig) => {
+        getAvailableMoves: (cordsConfig: Cords) => {
             let moveCors = []
-            const targetFigure = app.getFigureByCords(cordsConfig)
-            const addValue = targetFigure.color === 'white' ? 1 : -1
+            const targetFigure = Utility.getFigureByCords(cordsConfig)
 
-            moveCors.push({
-                ...cordsConfig,
-                y : cordsConfig.y + addValue
-            })
+                if(targetFigure) {
+                    const addValue = targetFigure.color === 'white' ? 1 : -1
 
-
-            if((targetFigure.color === 'black' && cordsConfig.y === 6) || (targetFigure.color === 'white' && cordsConfig.y === 1)) {
-                moveCors.push({
-                    ...cordsConfig,
-                    y : cordsConfig.y + 2 * addValue
-                })
+                    moveCors.push({
+                        ...cordsConfig,
+                        y : cordsConfig.y + addValue
+                    })
+        
+        
+                    if((targetFigure.color === 'black' && cordsConfig.y === 6) || (targetFigure.color === 'white' && cordsConfig.y === 1)) {
+                        moveCors.push({
+                            ...cordsConfig,
+                            y : cordsConfig.y + 2 * addValue
+                        })
+                    }
             }
 
             return moveCors
@@ -139,179 +147,86 @@ let chessPieces = {
 
 window.addEventListener('DOMContentLoaded', () => {
     bindUserUIActions()
-    app = new ChessGame
-    app.setBoard()
+    Board.setBoard()
 })
 
-function nodeListToArray(nodeList) {
-    return  Array.prototype.slice.call(nodeList)
-}
-
 function bindUserUIActions() {
-    document.querySelector('.mian-nav .new-game').addEventListener('click', () => {
-        app.restart()
-    })
+    const newGameButton = document.querySelector('.mian-nav .new-game') || false
+    const boardsElement = document.querySelector('.chess-board') || false
 
-    document.querySelector('.chess-board').addEventListener('click', (event) => {
-        let activeFigureClass = void (0)
-        let availableMoves = void (0)
-        const activeFigureElement = document.querySelector('.chess-figure.active')
-        const target = event.target
-        const figureElement = (target.classList.contains('figure-field') ? target.cloneNode(true) : target.parentElement).cloneNode(true)
-        const cordsConfig = app.getElementCords(target)
-        const targetFigureClass = app.getFigure(figureElement)
-
-        if(activeFigureElement) {
-            activeFigureClass = app.getFigure(activeFigureElement)
-            availableMoves = activeFigureClass ? activeFigureClass.getAvailableMoves(activeFigureClass.getFigureCords) : false
-        }
-
-        if(target.tagName === 'IMG') {
-            app.figureClicked(targetFigureClass, figureElement)
-        }
-
-        //console.log(typeof availableMoves, availableMoves)
-
-
-        if(activeFigureElement && availableMoves && availableMoves.filter(move => JSON.stringify(move) === JSON.stringify(cordsConfig)).length){
-            app.tryMoveFigure(cordsConfig)
-        }
-    })
-}
-
-class ChessGame {
-    restart() {
-        this.resetBoard()
-        this.setBoard()
+    if(newGameButton) {
+        newGameButton.addEventListener('click', () => {
+            Board.restart()
+        })
     }
 
-
-    resetBoard() {
-        document.querySelectorAll('.chess-board .figure-field').forEach((cell) => {
-            while(cell.lastChild) {
-                cell.removeChild(cell.lastChild)
+    if(boardsElement) {
+        boardsElement.addEventListener('click', event => {
+            const target = <HTMLElement>event.target || false
+            if(target && target.parentElement) {
+                let activeFigureClass: Piece | undefined;
+                let availableMoves: Cords[] | boolean = false
+                const activeFigureElement = <HTMLElement>document.querySelector('.chess-figure.active')
+                const figureElement = <HTMLElement>(target.classList.contains('figure-field') ? target.cloneNode(true) : target.parentElement).cloneNode(true)
+                const cordsConfig = Utility.getElementCords(target)
+                const targetFigureClass = Utility.getFigure(figureElement)
+        
+                if(activeFigureElement) {
+                    activeFigureClass = Utility.getFigure(activeFigureElement)
+                    if(activeFigureClass && activeFigureClass.getFigureCords) {
+                        availableMoves = activeFigureClass.getAvailableMoves(activeFigureClass.getFigureCords)
+                    }
+                }
+        
+                if(target.tagName === 'IMG' && targetFigureClass) {
+                    Utility.figureClicked(targetFigureClass, figureElement)
+                }
+        
+                if(cordsConfig && activeFigureElement && availableMoves && availableMoves.filter(move => JSON.stringify(move) === JSON.stringify(cordsConfig)).length){
+                    Utility.tryMoveFigure(cordsConfig)
+                }
             }
         })
     }
-
-    setBoard() {
-        document.querySelectorAll('.pieces-row .figure-field').forEach((tableDataElement) => {
-                let figureColor = tableDataElement.parentElement.dataset.startcolor
-                let figureType = tableDataElement.dataset.start
-                let figureSelector = figureColor + '-' + figureType
-
-                if(!['king', 'queen'].includes(figureType)) {
-                    let id = `-${nodeListToArray(tableDataElement.parentElement.querySelectorAll(`[data-start="${figureType}"]`)).indexOf( tableDataElement )}`;
-                    figureSelector += id
-                    figureType += id
-                }
-
-                let figure = new Piece(figureType, figureColor)
-
-                chessPieces.figures[figureSelector] = figure
-                tableDataElement.appendChild(figure.getPieceElement)                
-        })
-
-    }
-
-    figureClicked(figureClass, figureElement) {
-        app.deactivateFigures()
-
-        if(!figureClass.isActive && figureElement.classList.contains('active')) {  
-            app.deactivateFigure(figureClass)
-        } else {
-            app.activateFigure(figureClass)
-        }
-    }
-
-    tryMoveFigure(targetCords, availableMoves) {
-        const   activeFigureElement = document.querySelector('.chess-figure.active')
-                app.getFigure(activeFigureElement).move(targetCords)
-                activeFigureElement.classList.remove('active')
-    }
-
-    deactivateFigures() {
-        document.querySelectorAll('.chess-figure').forEach(singleElement => {
-           app.deactivateFigure(app.getFigure(singleElement))
-        })
-    }
-
-    deactivateFigure(figureClass) {
-        if(figureClass.isActive){
-            figureClass.isActive = false
-            figureClass.getFigureDOMElement.classList.remove('active')
-        }
-    }
-
-    activateFigure(figureClass) {
-        if(!figureClass.isActive){
-            figureClass.isActive = true
-            figureClass.getFigureDOMElement.classList.add('active')
-        }
-    }
-
-    getFigure(figureElement) {
-        return chessPieces.figures[figureElement.dataset.color + '-' + figureElement.dataset.type] || false
-    }
-
-    getElementCords(element) {
-        const rowElement = element.parentElement
-
-        return {
-            x: nodeListToArray(rowElement.querySelectorAll('.figure-field')).indexOf( element ),
-            y: nodeListToArray(rowElement.parentElement.querySelectorAll('.chess-row')).reverse().indexOf( rowElement )
-        }
-
-    }
-
-    getElemenyByCords(cordsConfig) {
-        return nodeListToArray(nodeListToArray(document.querySelectorAll('.chess-row')).reverse()[cordsConfig.y].querySelectorAll('.figure-field'))[cordsConfig.x] || false
-    }
-
-    getFigureByCords(cordsConfig) {
-        const figureHTMLElement = nodeListToArray(nodeListToArray(document.querySelectorAll('.chess-row')).reverse()[cordsConfig.y].querySelectorAll('.figure-field'))[cordsConfig.x]
-        let figure = void 0;
-
-        if(figureHTMLElement.firstChild) {
-            figure = this.getFigure(figureHTMLElement.firstChild) || false
-        } else {
-            return false
-        }
-
-        return figure
-    }
 }
 
-class Piece extends ChessGame {
-    constructor(pieceType, color) {
-        super();
-        this.pieceType = pieceType
-        this.color = color
-        this.isActive = true
-        this.getAvailableMoves = chessPieces[pieceType.replace(/-\d/, '')].getAvailableMoves
+export class Piece {
+    [x: string]: any
+    public getAvailableMoves: availableMoves
+    constructor(public pieceType: PieceType, public color: Color) {
+        this.getAvailableMoves = chessPieces[this.getPieceLabel].getAvailableMoves
     }
 
     logFigure() {
         console.log(this)
     }
 
-    move(cordsConfig) {
-        const figureElement = this.getFigureDOMElement
+    move(cordsConfig: Cords) {
         const figureCords = this.getFigureCords
-        const finishMoveSequence = () => {
-            this.getFigureDOMElement.style.transform = ''
-            this.getFigureDOMElement.classList.remove('transforming')
-            app.getElemenyByCords(cordsConfig).appendChild(this.getFigureDOMElement)
-            figureElement.removeEventListener('transitionend', finishMoveSequence)
+        if(figureCords) {
+            const figureElement = this.getFigureDOMElement
+            const finishMoveSequence = () => {
+                const figureElement = <HTMLElement>this.getFigureDOMElement
+                if(figureElement) {
+                    figureElement.style.transform = ''
+                    figureElement.classList.remove('transforming')
+                    Utility.getElemenyByCords(cordsConfig).appendChild(figureElement)
+                    figureElement.removeEventListener('transitionend', finishMoveSequence)
+                }
+            }
+    
+            figureElement.addEventListener('transitionend', finishMoveSequence)
+    
+            this.getFigureDOMElement.classList.add('transforming')
+            this.getFigureDOMElement.style.transform = `translateY(${100 * (figureCords.y - cordsConfig.y)}%) translateX(${100 * (cordsConfig.x - figureCords.x)}%)`
         }
-
-        figureElement.addEventListener('transitionend', finishMoveSequence)
-
-        this.getFigureDOMElement.classList.add('transforming')
-        this.getFigureDOMElement.style.transform = `translateY(${100 * (figureCords.y - cordsConfig.y)}%) translateX(${100 * (cordsConfig.x - figureCords.x)}%)`
     }
 
-    get getPieceElement() {
+    get getPieceLabel(): PieceType {
+        return <PieceType>this.pieceType.replace(/-\d/, '')
+    }
+
+    get getPieceElement(): HTMLDivElement {
         let pieceElement = document.createElement('div')
         let pieceImg = document.createElement('img')
 
@@ -319,22 +234,26 @@ class Piece extends ChessGame {
         pieceElement.dataset.color = this.color
         pieceElement.dataset.type = this.pieceType
 
-        pieceImg.src = chessPieces[this.pieceType] ? chessPieces[this.pieceType].asset : chessPieces[this.pieceType.replace(/-[0-9]*/, '')].asset
+        pieceImg.src = chessPieces[this.pieceType] ? chessPieces[this.pieceType].asset : chessPieces[this.getPieceLabel].asset
         pieceElement.appendChild(pieceImg)
 
         return pieceElement
     }
 
-    get getFigureDOMElement () {
-        return  document.querySelector(`[data-color="${this.color}"][data-type="${this.pieceType}"]`);
+    get getFigureDOMElement (): HTMLElement {
+        return  document.querySelector(`[data-color="${this.color}"][data-type="${this.pieceType}"]`) || document.body
     }
 
-    get getFigureCords() {
-        const rowElement = this.getFigureDOMElement.parentElement.parentElement
+    get getFigureCords(): Cords | undefined {
+        if(this.getFigureDOMElement) {
+            const rowElement:HTMLElement = this.getFigureDOMElement.closest('.chess-row') || document.body
 
-        return {
-            x: nodeListToArray(rowElement.querySelectorAll('.figure-field')).indexOf( this.getFigureDOMElement.parentElement ),
-            y: nodeListToArray(rowElement.parentElement.querySelectorAll('.chess-row')).reverse().indexOf( rowElement )
+            if(rowElement.parentElement) {
+                return {
+                    x: Utility.nodeListToArray(rowElement.querySelectorAll('.figure-field')).indexOf( this.getFigureDOMElement.parentElement ),
+                    y: Utility.nodeListToArray(rowElement.parentElement.querySelectorAll('.chess-row')).reverse().indexOf( rowElement )
+                }
+            }
         }
     }
 }
