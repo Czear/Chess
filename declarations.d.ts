4d6ea3b9a4b4e5d15301953ade6311e77fafa9af
Cezary Wakuliński <cezary.wakulinski@conversion.pl>
2019-09-20 23:52:06 +0200

    Add typescpirt interfaces for aplication

---
+++ declarations.d.ts
@@ -0,0 +1,42 @@
+interface Cords {
+    isEnemy?: boolean
+    x: number,
+    y: number,
+}
+
+interface Piece {
+    isActive?: boolean
+    pieceType: PieceType
+    color: Color
+    getPieceLabel: PieceType
+    getPieceElement: HTMLDivElement
+    getFigureDOMElement: HTMLElement
+    getFigureCords: Cords | undefined
+    getAvailableMoves: availableMoves
+    logFigure: () => void
+    move: (cordsConfig: Cords) => void
+}
+
+interface chessPieces {
+    king: PieceConfig
+    queen: PieceConfig
+    rook: PieceConfig
+    knight: PieceConfig
+    bishop: PieceConfig
+    pawn: PieceConfig
+    figures: {
+        [element: string]: Piece
+    }
+}
+
+interface PieceConfig {
+    asset: string,
+    getAvailableMoves: availableMoves
+}
+
+
+type availableMoves = (cords: Cords) => Cords[]
+type Axis = 'x' | 'y'
+type Color = 'black' | 'white'
+type Direction = 'up' | 'down'
+type PieceType = 'king' | 'queen' | 'rook' | 'bishop' | 'knight' | 'pawn'
